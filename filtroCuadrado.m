3e409632a8fab722568a948d35b5405e1834b2df
maskliv <donovan57ra@gmail.com>
2021-11-24 08:59:25 -0500

    creacion de filtro cuadrado

--- filtroCuadrado.m
+++ filtroCuadrado.m
@@ -1,7 +1,7 @@
-function [filtroResultado] = filtroCuadrado(paresDeCentros,radios,filterSize)
+function [filtroResultado] = filtroCuadrado(paresDeCentros,lados,filterSize)
 
 % Funcion para hacer filtros de naturaleza circular
-[numCircles, ~] = size(paresDeCentros);
+[numRects, ~] = size(paresDeCentros);
 
 h = filterSize(1,1);
 w = filterSize(1,2);
@@ -10,9 +10,9 @@ w = filterSize(1,2);
 [x,y] = meshgrid(1:w,1:h);
 
 filtro = false(filterSize);
-for i = 1:numCircles
-    circulo = (x-paresDeCentros(i,1)).^2 + (y-paresDeCentros(i,2)).^2 <= radios(i)^2;
-    filtro = circulo|filtro;
+for i = 1:numRects
+    rectangulo = abs(x-paresDeCentros(i,1))<=lados(i,1)/2 & abs(y-paresDeCentros(i,2))<=lados(i,2)/2;
+    filtro = rectangulo|filtro;
 end
 
 filtroResultado = double(filtro);
